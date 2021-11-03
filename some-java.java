6eb2b7008c2c5316124680ea76f599f0b02dc869
Magalam <rm250702@ncr.com>
2021-11-03 20:45:55 +0530

    added

---
+++ some-java.java
@@ -0,0 +1,7 @@
+import java.util.scanner;
+   public class consitionals {
+         public static void main(string[] args) {
+                scanner scanner =  new scanner(system.in);
+        system.out.println("hello world")
+         }
+   }
