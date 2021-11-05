6efe69265ad97d890ff886d1441fe9843a133e8d
raghugitty <62849630+raghugitty@users.noreply.github.com>
2021-11-05 20:00:21 +0530

    Delete some-java.java

--- some-java.java
+++
@@ -1,7 +0,0 @@
-import java.util.scanner;
-   public class consitionals {
-         public static void main(string[] args) {
-                scanner scanner =  new scanner(system.in);
-        system.out.println("hello world")
-         }
-   }
